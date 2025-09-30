87d5d497bd949268a3e01ecad6681da053ec16ad
ntica6 <nikolaticic5@gmail.com>
2025-09-30 15:01:51 -0500

    *Learned about import thread and parallel programming *Set up a learning environment for my it course where I will open pycharm, then chrome with the appropriate website, and finally I will give myself a reminder to take a study break

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import webbrowser
 import time
 from win10toast import ToastNotifier
 import random
+import threading
 
 toaster = ToastNotifier()
 
@@ -16,8 +17,14 @@ messages = [
 ]
 subprocess.Popen(["C:/Program Files/JetBrains/PyCharm 2025.2.1.1/bin/pycharm64.exe"])
 
-webbrowser.get("C:/Program Files/Google/Chrome/Application/chrome.exe %s").open("https://google.com")
+webbrowser.get("C:/Program Files/Google/Chrome/Application/chrome.exe %s").open("https://itskola.net")
 
-while True:
-    toaster.show_toast("Reminder!", random.choice(messages), duration = 1)
-    time.sleep(3)
+def write_reminder():
+    while True:
+        toaster.show_toast("Reminder!", random.choice(messages), duration = 1)
+        time.sleep(3)
+
+thread_write_reminder = threading.Thread(target = write_reminder)
+thread_write_reminder.start()
+
+print("Hello world")
